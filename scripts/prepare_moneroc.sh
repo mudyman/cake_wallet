3d2617a9b7e593299480d1b9152026c04c3ea348
mudyman <82431741+mudyman@users.noreply.github.com>
2025-06-22 16:24:53 +0000

    Update prepare_moneroc.sh

--- scripts/prepare_moneroc.sh
+++ scripts/prepare_moneroc.sh
@@ -6,10 +6,8 @@ cd "$(dirname "$0")"
 
 if [[ ! -d "monero_c/.git" ]];
 then
-    git clone https://github.com/amory1997/monero_c  monero_c
+    git clone https://github.com/mudyman/monero_c  monero_c
     cd monero_c
-    git checkout 0d93b1827b7c3b39583d57769824f75d710c8d00
-    git reset --hard
     git submodule update --init --force --recursive
     ./apply_patches.sh monero
     ./apply_patches.sh wownero
